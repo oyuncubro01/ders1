570452a26e68b3bc3f3113efdbbeaa7b5b507acf
oyuncubro01 <54633083+oyuncubro01@users.noreply.github.com>
2020-08-06 21:14:01 +0300

    Update ping.js

--- komutlar/ping.js
+++ komutlar/ping.js
@@ -1,13 +1,7 @@
 const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
-message.channel.send("**Pong 14**")
+message.channel.send("**OPPS**")
   
-}
-exports.conf = {
-  enabled: false,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 0,
 }
 exports.help = {
     name: 'ping'
